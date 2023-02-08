e29dab9fb123161e11de98392d056a7e638c11b4
my <jvckigh@gmail.com>
2023-02-08 22:34:08 +0900

    chore: init project

---
+++ src/main/java/com/mlmall/mlmallbe/MlmallBeApplication.java
@@ -0,0 +1,13 @@
+package com.mlmall.mlmallbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlmallBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MlmallBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mlmall/mlmallbe/MlmallBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlmall.mlmallbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlmallBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
